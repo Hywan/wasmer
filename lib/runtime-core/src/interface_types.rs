86945d4db7ce7602a5906359f9440f1bf24eda78
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-10-02 14:18:43 +0200

    fat(runtime-core) Continue to implement `wasmer-interface-types`.

--- lib/runtime-core/src/interface_types.rs
+++ lib/runtime-core/src/interface_types.rs
@@ -1,5 +1,5 @@
 use crate as core;
-use std::mem;
+use std::{collections::HashMap, mem, ops::Deref};
 use wasmer_interface_types::interpreter::wasm::{
     self as wit_wasm,
     values::{InterfaceType, InterfaceValue},
@@ -17,27 +17,59 @@ impl From<&core::types::Type> for InterfaceType {
     }
 }
 
-impl wit_wasm::structures::Export for core::export::Export {
-    fn inputs_cardinality(&self) -> usize {
-        match self {
-            core::export::Export::Function { signature, .. } => signature.params().len(),
-            _ => 0,
+#[allow(dead_code)]
+pub(crate) struct Export {
+    inner: core::export::Export,
+    inputs: Vec<InterfaceType>,
+    outputs: Vec<InterfaceType>,
+}
+
+impl From<core::export::Export> for Export {
+    fn from(export: core::export::Export) -> Self {
+        let inputs = match &export {
+            core::export::Export::Function { signature, .. } => {
+                signature.params().iter().map(Into::into).collect()
+            }
+            _ => vec![],
+        };
+        let outputs = match &export {
+            core::export::Export::Function { signature, .. } => {
+                signature.returns().iter().map(Into::into).collect()
+            }
+            _ => vec![],
+        };
+
+        Self {
+            inner: export,
+            inputs,
+            outputs,
         }
+    }
+}
+
+impl Deref for Export {
+    type Target = core::export::Export;
+
+    fn deref(&self) -> &core::export::Export {
+        &self.inner
+    }
+}
+
+impl wit_wasm::structures::Export for Export {
+    fn inputs_cardinality(&self) -> usize {
+        self.inputs.len()
     }
 
     fn outputs_cardinality(&self) -> usize {
-        match self {
-            core::export::Export::Function { signature, .. } => signature.returns().len(),
-            _ => 0,
-        }
+        self.outputs.len()
     }
 
     fn inputs(&self) -> &[InterfaceType] {
-        &[]
+        &self.inputs
     }
 
     fn outputs(&self) -> &[InterfaceType] {
-        &[]
+        &self.outputs
     }
 
     fn call(&self, _arguments: &[InterfaceValue]) -> Result<Vec<InterfaceValue>, ()> {
@@ -54,5 +86,62 @@ impl<'a> wit_wasm::structures::Memory<core::memory::MemoryView<'a, u8>> for core
         let length = self.size().bytes().0 / mem::size_of::<u8>();
 
         unsafe { core::memory::MemoryView::new(base as _, length as u32) }
+    }
+}
+
+#[allow(unused)]
+pub(crate) struct Instance<'a> {
+    inner: &'a core::instance::Instance,
+    exports: HashMap<String, Export>,
+}
+
+impl<'a> From<&'a core::instance::Instance> for Instance<'a> {
+    fn from(instance: &'a core::instance::Instance) -> Self {
+        Self {
+            inner: instance,
+            exports: instance
+                .exports()
+                .filter_map(|(export_name, export)| match export {
+                    core::export::Export::Function { .. } => {
+                        Some((export_name, export.clone().into()))
+                    }
+                    _ => None,
+                })
+                .collect(),
+        }
+    }
+}
+
+impl Deref for Instance<'_> {
+    type Target = core::instance::Instance;
+
+    fn deref(&self) -> &core::instance::Instance {
+        self.inner
+    }
+}
+
+impl<'instance>
+    wit_wasm::structures::Instance<
+        Export,
+        (),
+        core::memory::Memory,
+        core::memory::MemoryView<'_, u8>,
+    > for Instance<'instance>
+{
+    fn export(&self, export_name: &str) -> Option<&Export> {
+        self.exports.get(export_name)
+    }
+
+    fn local_or_import<
+        I: wit_wasm::structures::TypedIndex + wit_wasm::structures::LocalImportIndex,
+    >(
+        &self,
+        _index: I,
+    ) -> Option<&()> {
+        None
+    }
+
+    fn memory(&self, _index: usize) -> Option<&core::memory::Memory> {
+        None
     }
 }
