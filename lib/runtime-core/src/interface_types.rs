0bec8f0db3764dee64961d1b52662e2a76195ee5
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-10-04 12:08:10 +0200

    feat(runtime-core) Read export and imports types from WIT.

--- lib/runtime-core/src/interface_types.rs
+++ lib/runtime-core/src/interface_types.rs
@@ -1,7 +1,7 @@
 use crate::{self as core, structures::TypedIndex};
 use std::{collections::HashMap, mem, ops::Deref, sync::Arc};
-use wasmer_interface_types::interpreter::wasm as wit_wasm;
 pub use wasmer_interface_types::interpreter::wasm::values::{InterfaceType, InterfaceValue};
+use wasmer_interface_types::{self as wit, interpreter::wasm as wit_wasm};
 
 pub mod interpreter {
     pub use wasmer_interface_types::interpreter::{Instruction, Interpreter};
@@ -61,15 +61,15 @@ pub struct Export<'function> {
     outputs: Vec<InterfaceType>,
 }
 
-impl<'function> From<core::instance::DynFunc<'function>> for Export<'function> {
-    fn from(export: core::instance::DynFunc<'function>) -> Self {
-        let inputs = export.signature.params().iter().map(Into::into).collect();
-        let outputs = export.signature.returns().iter().map(Into::into).collect();
-
+impl<'function, 'interfaces> Export<'function> {
+    fn new(
+        export_callable: core::instance::DynFunc<'function>,
+        wit_export: &'interfaces wit::ast::Export<'interfaces>,
+    ) -> Self {
         Self {
-            inner: export,
-            inputs,
-            outputs,
+            inner: export_callable,
+            inputs: wit_export.input_types.clone(),
+            outputs: wit_export.output_types.clone(),
         }
     }
 }
@@ -125,16 +125,27 @@ impl LocalImport {
     fn new(
         function_index: core::types::FuncIndex,
         module: Arc<core::module::ModuleInner>,
+        wit_import: Option<&wit::ast::ImportedFunction>,
     ) -> Result<Self, &'static str> {
-        let signature_index = module
-            .info
-            .func_assoc
-            .get(function_index)
-            .ok_or_else(|| "Invalid function index.")?;
-        let signature = core::sig_registry::SigRegistry
-            .lookup_signature_ref(&module.info.signatures[*signature_index]);
-        let inputs = signature.params().iter().map(Into::into).collect();
-        let outputs = signature.returns().iter().map(Into::into).collect();
+        let (inputs, outputs) = match wit_import {
+            Some(wit_import) => (
+                wit_import.input_types.clone(),
+                wit_import.output_types.clone(),
+            ),
+            None => {
+                let signature_index = module
+                    .info
+                    .func_assoc
+                    .get(function_index)
+                    .ok_or_else(|| "Invalid function index.")?;
+                let signature = core::sig_registry::SigRegistry
+                    .lookup_signature_ref(&module.info.signatures[*signature_index]);
+                let inputs = signature.params().iter().map(Into::into).collect();
+                let outputs = signature.returns().iter().map(Into::into).collect();
+
+                (inputs, outputs)
+            }
+        };
 
         Ok(Self {
             function_index,
@@ -179,60 +190,98 @@ impl<'a> wit_wasm::structures::Memory<core::memory::MemoryView<'a, u8>> for core
     }
 }
 
-pub struct Instance<'a> {
-    inner: &'a core::instance::Instance,
-    exports: HashMap<String, Export<'a>>,
+pub struct Instance<'instance, 'interfaces> {
+    inner: &'instance core::instance::Instance,
+    exports: HashMap<String, Export<'instance>>,
     locals_imports: HashMap<usize, LocalImport>,
     memories: Vec<core::memory::Memory>,
+    #[allow(unused)]
+    interfaces: &'interfaces wit::ast::Interfaces<'interfaces>,
 }
 
-impl<'a> From<&'a core::instance::Instance> for Instance<'a> {
-    fn from(instance: &'a core::instance::Instance) -> Self {
-        let exports = instance
-            .module
-            .info
+impl<'instance, 'interfaces> Instance<'instance, 'interfaces> {
+    pub fn new(
+        core_instance: &'instance core::instance::Instance,
+        interfaces: &'interfaces wit::ast::Interfaces,
+    ) -> Result<Self, &'static str> {
+        let module_info = &core_instance.module.info;
+
+        let exports = module_info
             .exports
             .iter()
             .filter_map(|(export_name, export_index)| match export_index {
                 core::module::ExportIndex::Func(..) => Some((
                     export_name.to_owned(),
-                    instance
-                        .dyn_func(export_name)
-                        .expect(&format!(
+                    Export::new(
+                        core_instance.dyn_func(export_name).expect(&format!(
                             "Failed to get a dynamic function for `{}`.",
                             export_name
-                        ))
-                        .into(),
+                        )),
+                        interfaces
+                            .exports
+                            .iter()
+                            .find(|export| export.name == export_name)?,
+                    ),
                 )),
                 _ => None,
             })
             .collect();
 
-        let mut memories: Vec<core::memory::Memory> = instance
+        let imports = module_info
+            .imported_functions
+            .iter()
+            .filter_map(|(imported_index, import_name_indexes)| {
+                let namespace = module_info
+                    .namespace_table
+                    .get(import_name_indexes.namespace_index);
+                let name = module_info.name_table.get(import_name_indexes.name_index);
+
+                match interfaces
+                    .imported_functions
+                    .iter()
+                    .find(|imported_function| {
+                        imported_function.namespace == namespace && imported_function.name == name
+                    }) {
+                    Some(interface_imported_function) => Some((
+                        imported_index.index(),
+                        LocalImport::new(
+                            imported_index.convert_up(module_info),
+                            core_instance.module.clone(),
+                            Some(interface_imported_function),
+                        )
+                        .ok()?,
+                    )),
+                    None => None,
+                }
+            })
+            .collect();
+
+        let mut memories = core_instance
             .exports()
             .filter_map(|(_, export)| match export {
                 core::export::Export::Memory(memory) => Some(memory.to_owned()),
                 _ => None,
             })
-            .collect();
+            .collect::<Vec<core::memory::Memory>>();
 
-        if let Some(core::export::Export::Memory(memory)) = instance
+        if let Some(core::export::Export::Memory(memory)) = core_instance
             .import_object
             .maybe_with_namespace("env", |env| env.get_export("memory"))
         {
             memories.push(memory);
         }
 
-        Self {
-            inner: instance,
+        Ok(Self {
+            inner: core_instance,
             exports,
             memories,
-            locals_imports: HashMap::new(),
-        }
+            locals_imports: imports,
+            interfaces,
+        })
     }
 }
 
-impl Deref for Instance<'_> {
+impl Deref for Instance<'_, '_> {
     type Target = core::instance::Instance;
 
     fn deref(&self) -> &Self::Target {
@@ -240,13 +289,13 @@ impl Deref for Instance<'_> {
     }
 }
 
-impl<'instance>
+impl<'instance, 'interfaces>
     wit_wasm::structures::Instance<
         Export<'instance>,
         LocalImport,
         core::memory::Memory,
         core::memory::MemoryView<'_, u8>,
-    > for Instance<'instance>
+    > for Instance<'instance, 'interfaces>
 {
     fn export(&self, export_name: &str) -> Option<&Export<'instance>> {
         self.exports.get(export_name)
@@ -262,7 +311,8 @@ impl<'instance>
 
         if !self.locals_imports.contains_key(&index) {
             let function_index = core::types::FuncIndex::new(index);
-            let local_import = LocalImport::new(function_index, self.module.clone()).ok()?;
+            let local_import =
+                LocalImport::new(function_index, self.inner.module.clone(), None).ok()?;
 
             self.locals_imports.insert(index, local_import);
         }
